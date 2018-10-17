b9a7c5e72adf988f1428d0c2b39573b27f2b665b
Alicia Geijer <0002alge@res.ludvika.intra>
2018-10-17 09:37:01 +0200

    lagt till spel logik

--- src/js/memory.js
+++ src/js/memory.js
@@ -1,6 +1,4 @@
 // TODO: 5.Brickorna måste blandas
-// TODO: 7. 2 lika brickor tas bort.
-// TODO: 8. 2 olika brickor vänds tillbaka.
 // TODO: 9. När 2 brickor vänds ska antalet försök uppdateras.
 // TODO: 10. Antalet försök och antalet par ska visas för användaren.
 // TODO: 11. Antalet sekunder spelat ska också visas.
@@ -11,6 +9,43 @@ genom att anropa en funktion flera gånger. */
 skriven i markup språket Markdown. Bör innehålla kortare information
 om vad som ligger i respektive fil samt vilka kommandon som ska
 köras för att starta utvecklingsserver samt hur man bygger en build. */
+
+const turnBrick = (bricks, img) => {
+  // FIXME: fixa till sen
+  // kolla om en brick redan är vänd
+  if (bricks.first === null) {
+    bricks.first = img;
+  } else {
+    bricks.second = img;
+
+    // kolla om vi har ett par
+    if (bricks.first.getAttribute('src') === bricks.second.getAttribute('src')) {
+      // FIXME: skriv om sen
+      // göm brickor
+      const removeBrick = () => {
+        bricks.first.parentElement.classList.add('hidden');
+        bricks.second.parentElement.classList.add('hidden');
+        // reset brick logik
+        bricks.first = null;
+        bricks.second = null;
+      };
+      window.setTimeout(removeBrick, 100);
+    } else {
+      // FIXME: fixa sen
+      const turnBackBrick = () => {
+        // sökväg till bilden för baksida
+        const path = 'images/0.png';
+        // vänd tillbaka brickor
+        bricks.first.setAttribute('src', path);
+        bricks.second.setAttribute('src', path);
+        // reset brick logik
+        bricks.first = null;
+        bricks.second = null;
+      };
+      window.setTimeout(turnBackBrick, 100);
+    }
+  }
+};
 
 const renderMemory = (containerId, bricks) => {
   // container element
@@ -27,7 +62,7 @@ const renderMemory = (containerId, bricks) => {
 
   // FIXME: skriv om senare
   // loop för att skriva ut brickor
-  for (let i = 0; i < brick.tiles.length; i++) {
+  for (let i = 0; i < bricks.tiles.length; i++) {
     // FIXME: bör skrivas om senare
     // event hanterare funktion
     const handleClick = event => {
@@ -40,7 +75,11 @@ const renderMemory = (containerId, bricks) => {
       }
       const path = `images/${bricks.tiles[i]}.png`;
       img.setAttribute('src', path);
+
+      // vänd bricka
+      turnBrick(bricks, img);
     };
+
     // lägger in bricka
     const brick = document.importNode(templateDiv.firstElementChild, true);
     // FIXME: bör skrivas om senare
@@ -59,6 +98,8 @@ const memory = () => {
 
   // brick logik
   const bricks = {
+    first: null,
+    second: null,
     tiles: [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8]
   };
 
